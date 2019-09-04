79b982e9ab7075da76c4277e9e18e5317ed9f229
Mohit Bajaj <mohit_b35@hotmail.com>
2019-09-04 19:28:47 +0530

    Adjustments for responsive fixed navbar

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,15 +1,15 @@
 function menuToggle(event) {
 	const navbarSecond = document.querySelectorAll('.navbar-second');
 	const menuButton = event.target;
-	
+
 	navbarSecond.forEach( nav => {
 		nav.classList.toggle('navbar-second-hidden');
 	})
 
 	if(navbarSecond[0].classList.contains('navbar-second-hidden')){
-		menuButton.innerText = "Show Menu";
+		menuButton.textContent = "Show Menu";
 	} else {
-		menuButton.innerText = "Hide Menu";
+		menuButton.textContent = "Hide Menu";
 	}
 }
 
