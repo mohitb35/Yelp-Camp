c9445b15c51acfb19f14a4becd5edbe53e16bc25
Mohit Bajaj <mohit_b35@hotmail.com>
2019-09-06 13:31:42 +0530

    <main> > EJS + added create/find camp mongodb code

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 // Dependencies
-const express = require('express');
-const ejs = require('ejs');
-const bodyParser = require('body-parser');
+const 	express = require('express'), 
+		app = express(),
+		bodyParser = require('body-parser'), 
+		mongoose = require('mongoose');
 
-const app = express();
+mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
 
 // Letting Express know that we're serving up ejs templates by default.
 app.set('view engine', 'ejs');
@@ -11,8 +12,17 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Dummy Campgrounds array set up
-let campgrounds = [
+// DB Schema set up
+let campgroundSchema = mongoose.Schema({
+	name: String,
+	image: String
+});
+
+// DB Model setup
+let Campground = mongoose.model("Campground", campgroundSchema);
+
+// Dummy Campgrounds array set up - Can be removed
+/* let campgrounds = [
 	{name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
 	{name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
 	{name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
@@ -22,7 +32,7 @@ let campgrounds = [
 	{name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
 	{name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
 	{name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"}
-];
+]; */
 
 // Routes
 // Landing Page
@@ -32,16 +42,30 @@ app.get('/', (req, res) => {
 
 // Campgrounds
 app.get('/campgrounds', (req, res) => {
-	res.render('campgrounds', {campgrounds: campgrounds});
+	// Get campgrounds from DB
+	Campground.find({}, (err, allCampgrounds) => {
+		if(err) {
+			console.log("Error", err);
+		} else {
+			res.render('campgrounds', {campgrounds: allCampgrounds});
+		}
+	})
 });
 
 app.post('/campgrounds', (req, res) => {
+	// Create new campground and save to DB
 	let newCampground = {
 		name: req.body.name,
 		image: req.body.image
 	}
-	campgrounds.push(newCampground);
-	res.redirect('campgrounds');
+	Campground.create(newCampground, (err, createdCampground) => {
+		if(err) {
+			console.log("Error", err);
+		} else {
+			console.log("New campground:", createdCampground);
+			res.redirect('campgrounds');
+		}
+	})
 });
 
 app.get('/campgrounds/new', (req, res) => {
@@ -53,5 +77,5 @@ app.get('/campgrounds/new', (req, res) => {
 // Setting up server to listen at port 3000, or PORT value set as environment (if available)
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-	console.log('Server started');
+	console.log('Server started', '-', new Date().toLocaleString());
 });
